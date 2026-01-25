cc7ef23403c4d738e842a3301b00d521b5cbda6f
amdreiso <andreiscatolin@gmail.com>
2026-01-24 22:40:27 -0300

    added language code list

--- data.py
+++ data.py
@@ -2,11 +2,14 @@
 import json
 import random
 
+LANGUAGE_CODES = ["aa","ab","af","ak","sq","am","ar","an","hy","as","av","ae","ay","az","ba","bm","eu","be","bn","bi","bo","bs","br","bg","my","ca","cs","ch","ce","zh","cu","cv","kw","co","cr","cy","cs","da","de","dv","nl","dz","el","en","eo","et","eu","ee","fo","fa","fj","fi","fr","fr","fy","ff","ka","de","gd","ga","gl","gv","el","gn","gu","ht","ha","he","hz","hi","ho","hr","hu","hy","ig","is","io","ii","iu","ie","ia","id","ik","is","it","jv","ja","kl","kn","ks","ka","kr","kk","km","ki","rw","ky","kv","kg","ko","kj","ku","lo","la","lv","li","ln","lt","lb","lu","lg","mk","mh","ml","mi","mr","ms","mk","mg","mt","mn","mi","ms","my","na","nv","nr","nd","ng","ne","nl","nn","nb","no","ny","oc","oj","or","om","os","pa","fa","pi","pl","pt","ps","qu","rm","ro","ro","rn","ru","sg","sa","si","sk","sk","sl","se","sm","sn","sd","so","st","es","sq","sc","sr","ss","su","sw","sv","ty","ta","tt","te","tg","tl","th","bo","ti","to","tn","ts","tk","tr","tw","ug","uk","ur","uz","ve","vi","vo","cy","wa","wo","xh","yi","yo","za","zh","zu"]
+
 class Language:
-    def __init__(self, name) -> None:
+    def __init__(self, name, code="en") -> None:
         self.token_list = []
         self.streak = 0
         self.name = name
+        self.code = code
 
     def add(self, native, translation):
         for token in self.token_list:
@@ -46,6 +49,7 @@ class Language:
             data = {}
             data["name"] = self.name
             data["streak"] = self.streak
+            data["code"] = self.code
             data["token_list"] = [t.to_dict() for t in self.token_list]
             json.dump(data, f)
 
@@ -55,6 +59,7 @@ class Language:
 
         lang = Language(dt["name"])
         lang.streak = dt["streak"]
+        lang.code = dt["code"]
         lang.token_list = [Token.from_dict(d) for d in dt["token_list"]]
         return lang
 
